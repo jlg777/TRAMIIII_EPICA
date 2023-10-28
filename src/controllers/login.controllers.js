34106c63cb5ca60129f56db9f2735e0501d0743e
jlg777 <proyectojlg777@gmail.com>
2023-10-28 19:41:26 -0300

    se agrega modelo de datos - models

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,14 +1,33 @@
+//importo provisoriamente
+import { loginUser } from "../models/login-model.js"; 
+
 
 export const ctrlGetLogin = (req, res) => {
     try {
-        res.send('Hello World!')
+        res.status(200).json(loginUser)
     } catch (error) {
         console.error(error)
         return res.status(500).json({
             message: 'Error Server'
         })
+    } finally {
+        //console.log('')
     }
 };
+
+
+/*export const ctrlGetLogin = (req, res) => {
+    try {
+        throw new Error('BROKEN') 
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'Error Server'
+        })
+    } finally {
+        //console.log('')
+    }
+};*/
 
 export const ctrlPostLogin = (req, res) => {
     try {
@@ -19,6 +38,8 @@ export const ctrlPostLogin = (req, res) => {
         return res.status(500).json({
             message: 'Error Server'
         })
+    } finally {
+        //console.log('')
     }
 };
 
@@ -30,6 +51,8 @@ export const ctrlPutLogin =  (req, res) => {
         return res.status(500).json({
             message: 'Error Server'
         })   
+    } finally {
+        //console.log('')
     }
 };
 
@@ -41,6 +64,7 @@ export const ctrlDelLogin =  (req, res) => {
         return res.status(500).json({
             message: 'Error Server'
         })    
+    }finally {
+        //console.log('')
     }
-    
 };
---
+++ src/models/login-model.js
@@ -0,0 +1,12 @@
+export const loginUser =  [
+    {
+        user: "correo@domain.com",
+        name:"",
+        image:""
+    },
+    {
+        user: "correo@domain.com",
+        name:"",
+        image:""
+    }
+ ] 
