ab7c6758aa40a53b89e3da863e3ffc820de7b209
jlg777 <proyectojlg777@gmail.com>
2023-11-01 11:39:10 -0300

    se configura el buscar by id

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,12 +1,12 @@
 //importo provisoriamente
-import { loginUser, emailModel } from "../models/login-model.js"; 
+import { userModel } from "../models/login-model.js"; 
 
-
-
-export const ctrlGetLogin = (req, res, next) => {
+export const ctrlPostLogin = (req, res, next) => {
     try {
-        res.status(200).json(loginUser)
-        //res.sendFile('index.html')
+       console.log(req.body)
+       userModel.create(req.body)
+        //res.status(201)
+        res.send('Got a POST request')
     } catch (error) {
         next(error)
     } finally {
@@ -14,20 +14,30 @@ export const ctrlGetLogin = (req, res, next) => {
     }
 };
 
-
-export const ctrlGetLoginId = (req, res, next) => {
+export const ctrlGetLogin = (req, res, next) => {
     try {
-        const {logid} = req.params
-            console.log(logid)
-            console.log(loginUser)
-        //const login = loginUser.find(logid);
-        //return res.status(200).json(login)
-        //res.sendFile('index.html')
+        const users = userModel.findAll();
+        res.status(200).json(users)
     } catch (error) {
         next(error)
     } finally {
         //console.log('')
     }
+};
+
+export const ctrlGetLoginId = (req, res, next) => {
+    try {
+        const { logid } = req.params;
+        const user = userModel.findOne({ id: logid });
+        if (!user) {
+            return res.sendStatus(404);
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        next(error)
+    } finally {
+        //console.log('')
+    }  
 };
 
 /*export const ctrlGetLogin = (req, res, next) => {
@@ -40,20 +50,7 @@ export const ctrlGetLoginId = (req, res, next) => {
     }
 };*/
 
-export const ctrlPostLogin = (req, res, next) => {
-    try {
-       console.log(req.body)
-       emailModel.create(req.body)
-        //res.status(201)
-        res.send('Got a POST request')
-    } catch (error) {
-        next(error)
-    } finally {
-        //console.log('')
-    }
-};
-
-export const ctrlPutLogin =  (req, res, next) => {
+/*export const ctrlPutLogin =  (req, res, next) => {
     try {
         res.send('Got a PUT request at /user')
     } catch (error) {
@@ -61,9 +58,9 @@ export const ctrlPutLogin =  (req, res, next) => {
     } finally {
         //console.log('')
     }
-};
+};*/
 
-export const ctrlDelLogin =  (req, res, next) => {
+/*export const ctrlDelLogin =  (req, res, next) => {
     try {
         res.send('Got a DELETE request at /user')
     } catch (error) {
@@ -71,7 +68,7 @@ export const ctrlDelLogin =  (req, res, next) => {
     }finally {
         //console.log('')
     }
-};
+};*/
 
 
 
--- src/models/login-model.js
+++ src/models/login-model.js
@@ -1,31 +1,31 @@
 
-let listUser = [];
-
-export const loginUser =  [
+let listUser = [
     {
-        id: Date.now(),
+        id: 123,
         name: "correo@domain.com",
         email:"",
         pass:""
     },
-    {
-        name: "correo@domain.com",
-        email:"",
-        pass:""
-    }
- ];
+];
 
- const createNewUser = ({ name, email, pass }) => {
-    if (!name) return null;
-  
-    const newLogin = { id: Date.now(), name, email, pass };
-  
-    loginUser.push(newLogin);
-  
-    return newLogin;
-  };
+const createNewUser = ({ name, email, pass }) => {
+  if (!name) return null;
+  const newLogin = { id: Date.now(), name, email, pass };
+  listUser.push(newLogin);
+  return newLogin;
+};
 
+const GetAllUsers = () => {
+  return [...listUser];
+};
 
-  export const emailModel = {
-    create: createNewUser,
-  };
+const getUserById = ({ id }) => {
+  const post = listUser.find((post) => post.id == id);
+  return post;
+};
+
+export const userModel = {
+  create: createNewUser,
+  findAll: GetAllUsers,
+  findOne: getUserById,
+};
